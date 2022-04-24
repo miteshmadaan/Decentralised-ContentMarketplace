425485a1fcec5d68e89f20911fb134f22a578971
"MITESH MADAAN" <"miteshmadaan1@gmail.com">
2022-04-24 18:49:06 +0530

    mostly working

--- contracts/myContract.sol
+++ contracts/myContract.sol
@@ -7,33 +7,60 @@ contract testContract {
     
     mapping(uint => subscriber) public subscribers;
     uint public subscriberCount = 0;
- 
+
     struct subscriber {
         string name;
         address addr;
     }
 
-    address public author;
+    address payable public author;
+    uint public subscriptionFee;
 
-    constructor(){
-        author = msg.sender;
+    event SubscriberPrint(string name, address addr);
+    event Received(string name, address addr, uint amount);
+
+    constructor(uint fee){
+        author = payable(msg.sender);
+        subscriptionFee = fee * 1e18;
     }
 
-    function addSubscriber(string memory userName) public {
+    function getContractBalance() public view returns (uint){
+        return address(this).balance;
+    }
+
+    fallback() payable external{
+        require( msg.value ==  subscriptionFee , "Payment should be the subscription fee"    );
+    }
+
+    receive() payable external{
+        require( msg.value ==  subscriptionFee , "Payment should be the subscription fee"    );
+        // emit Received(msg.sender, msg.value);
+    }
+
+    function getAuthorBalance() public view returns(uint){
+        return author.balance;
+    }
+
+    function addSubscriber(string memory userName) public payable{
         subscribers[subscriberCount] = subscriber(userName,msg.sender);
         subscriberCount++;
+        payable(address(this)).transfer(msg.value);
+        emit Received(userName, msg.sender, msg.value);
     }
  
-    function getSubscriberCount() public view returns(uint256) {
-        return subscriberCount;
+    function withdrawAmount() payable public{
+        require(
+            msg.sender == author, "Only author can call!"
+        );
+        payable(author).transfer(address(this).balance);
     }
 
-    function printSubscribersList() public view returns(subscriber[]memory){
-        subscriber[] memory ret = new subscriber[](subscriberCount);
+    function printSubscribersList() public {
+        subscriber memory ret;
         for(uint i = 0; i < subscriberCount; i++){
-            ret[i] = subscribers[i];
+            ret = subscribers[i];
+            emit SubscriberPrint(ret.name, ret.addr);
         }
-        return ret;
     }
 
 }
